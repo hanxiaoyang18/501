9b7b1942a5646dcd01a4f436d111c0577b07b077
hanxiaoyang18 <xiaoyang.han@etu.unilim.fr>
2023-12-04 15:55:09 +0100

    501

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Head from "./components/Head";
+
 export default function App() {
   return (
+    <>
+    <Head></Head>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </>
   )
 }
---
+++ src/components/Head.jsx
@@ -0,0 +1,29 @@
+
+export default function Head() {
+
+    return (
+      <>
+      
+        <div className="text-2xl">
+          <button className="m-4 ">
+           HAUTE COUTURE
+          </button>
+          <button className="m-4 ">
+            MODE
+          </button>
+          <button className="m-4 ">
+          HAUTE JOAILLERIE
+          </button>
+          <button className="m-4 ">
+           LUNETTES
+          </button>
+          <button className="m-4">
+            MAQUILLAGE
+          </button>
+          <button className="m-4 ">
+           SOIN
+          </button>
+        </div>
+      </>
+    );
+  }
