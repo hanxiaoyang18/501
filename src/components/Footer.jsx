0e79dea1e928eb47135b8c68aebcb9690187706a
hanxiaoyang18 <xiaoyang.han@etu.unilim.fr>
2023-12-21 12:01:23 +0100

    501

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+    return (
+        <div className=" h-[10vw] bg-slate-50 ">
+
+            
+
+            <h1>
+
+                Les conseillers CHANEL sont à votre disposition pour toute information du lundi au samedi, de 10h00 à 19h00.
+                Vous pouvez nous contacter par email ou appeler le+ 33 9 75 18 15 09.
+
+            </h1>
+
+            <h1>Saisissez un lieu pour trouver les boutiques CHANEL les plus proches</h1>
+
+            <h1>Abonnez-vous pour suivre toute l’actualité de la Maison CHANEL</h1>
+        </div>
+
+
+
+
+    );
+}
